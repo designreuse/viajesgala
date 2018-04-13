d2b2f92502e637c4845d67bd62b5469054f81c2f
estopon <estopon@gmail.com>
2018-04-13 11:49:29 +0200

    Initial commit

---
+++ src/main/java/com/viajesgala/ViajesGala.java
@@ -0,0 +1,12 @@
+package com.viajesgala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViajesGala {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViajesGala.class, args);
+	}
+}
