2ad7ce0ddd473e1f8884fc3a892bbba94a5555a7
estopon <estopon@gmail.com>
2018-05-13 23:01:04 +0200

    Lista de categorías

--- src/main/java/com/viajesgala/controllers/HomeController.java
+++ src/main/java/com/viajesgala/controllers/HomeController.java
@@ -1,6 +1,9 @@
 package com.viajesgala.controllers;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.viajesgala.services.WordPressService;
+import com.viajesgala.wpjson.Category;
 import com.viajesgala.wpjson.Post;
 
 @Controller
@@ -20,7 +24,17 @@ public class HomeController {
 	public String home(Model model) {
 		
 		List<Post> posts = wordPressService.getPosts();
+		Set<String> categoriesSet = new HashSet<>();
+		if (posts != null) {
+			for (Post post: posts) {
+				for (Category category: post.getTerms().getCategory()) {
+					categoriesSet.add(category.getName());
+				}
+			}
+		}
+		List<String> categories = new ArrayList<String>(categoriesSet);
 		model.addAttribute("posts",posts);
+		model.addAttribute("categories",categories);
 		
 		return "home";
 	}
