a05f72858f18d6156d7138f59d3b4b463359ebf6
estopon <estopon@gmail.com>
2018-07-26 19:48:46 +0200

    Filtro de posts por categoría

--- src/main/java/com/viajesgala/controllers/HomeController.java
+++ src/main/java/com/viajesgala/controllers/HomeController.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.viajesgala.services.WordPressService;
 import com.viajesgala.wpjson.Category;
@@ -39,10 +41,22 @@ public class HomeController {
 		return "home";
 	}
 	
-	@GetMapping("/posts")
-	public String posts(Model model) {
+	@GetMapping("/posts/{category}")
+	public String posts(Model model,@PathVariable String category) {
 		List<Post> posts = wordPressService.getPosts();
-		model.addAttribute("posts",posts);
+		List<Post> postsFiltered = new ArrayList<Post>();
+		
+		if (posts != null) {			
+			for (Post post:posts) {				
+				List<Category> categories = post.getTerms().getCategory().stream().filter(c->c.getName().equals(category)).collect(Collectors.toList());
+				if (categories!=null && categories.size()>0) {
+					postsFiltered.add(post);
+				}
+				
+			}
+		}
+		
+		model.addAttribute("posts",postsFiltered);
 		
 		return "posts";
 		
