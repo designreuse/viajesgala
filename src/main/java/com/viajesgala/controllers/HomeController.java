73a5bf8a49d3518725edca809370eeab3f564877
eduardosanchezbonilla <eduardosanchezbonilla@gmail.com>
2018-12-19 16:09:44 +0100

    Prueba rama

--- src/main/java/com/viajesgala/controllers/HomeController.java
+++ src/main/java/com/viajesgala/controllers/HomeController.java
@@ -135,7 +135,7 @@ public class HomeController {
 		emailService.sendSimpleMessage(springMailUsername, 
 									   asuntoMailFormulario, 
 									   mensaje
-									  );			
+									  );   			
 		return "OK";				
 	}
 	
