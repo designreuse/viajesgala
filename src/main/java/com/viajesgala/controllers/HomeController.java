7a1168f7c10f36dadeb861b6c744a9ec9f9b6884
Eduardo Sánchez Bonilla <eduardosanchezbonilla@gmail.com>
2018-12-25 20:50:34 +0100

    Contenido Home

--- src/main/java/com/viajesgala/controllers/HomeController.java
+++ src/main/java/com/viajesgala/controllers/HomeController.java
@@ -15,7 +15,6 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
