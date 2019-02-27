2694f6d05f9e11ac04a0464cced7d5e436f47b62
Eduardo Sánchez Bonilla <eduardosanchezbonilla@gmail.com>
2019-02-27 09:36:00 +0100

    War file

--- src/main/java/com/viajesgala/ViajesGala.java
+++ src/main/java/com/viajesgala/ViajesGala.java
@@ -2,12 +2,19 @@ package com.viajesgala;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-public class ViajesGala {
+public class ViajesGala extends SpringBootServletInitializer{
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ViajesGala.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ViajesGala.class, args);
 	}
